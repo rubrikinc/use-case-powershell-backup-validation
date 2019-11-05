b6fab3efaffb70149ab72ac3c1eac27492c1975f
cleygraf <christoph@leygraf.de>
2019-11-05 09:14:12 +0100

    Add more tests

--- tests.ps1
+++ tests.ps1
@@ -3,15 +3,42 @@ param(
     [System.Management.Automation.PSCredential]$GuestCredential
 )
 
+# 
+# Tests running on the local machine, e. g. try to ping the test vm
+#
 task Ping {
-    assert (Test-Connection -ComputerName $Config.testIp -Quiet) "Unable to ping the server."
-    
+    $progressPreference = 'SilentlyContinue';
+    Test-Connection $Config.testIp -Quiet 6> $null
+    assert (Test-Connection $Config.testIp -Quiet 6> $null) "Unable to ping the server."
 }
 
-task Netlogon {
-    $GuestCredentialModified = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList ('.\'+$GuestCredential.UserName), ($GuestCredential.Password)
-    $ValidateService = (Get-WmiObject -Class Win32_Service -ComputerName $Config.testIp -Credential $GuestCredentialModified -Filter "name='Netlogon'").State
-    equals $ValidateService 'Running'
+task Port80 {
+    # Test-NetConnection seems not be available on macOS
+    assert (Test-Connection $Config.testIp -TCPPort 80) "Unable to connect to port 80 of the server."
 }
 
+#
+# Tests running inside the test vm, e. g. check if a service is running. VMware-tools are used to achieve this.
+#
+task NTDS {
+    $splat = @{
+        ScriptText      = 'if ( Get-Service "NTDS" -ErrorAction SilentlyContinue ) { Write-Output "running" } else { Write-Output "not running" }'
+        ScriptType      = 'PowerShell'
+        VM              = $Config.mountName
+        GuestCredential = $GuestCredential
+    }  
+    $output = Invoke-VMScript @splat -ErrorAction Stop
+    equals "$($output.Trim())" "running"
+}
+
+task MSSQLSERVER {
+    $splat = @{
+        ScriptText      = 'if ( Get-Service "MSSQLSERVER" -ErrorAction SilentlyContinue ) { Write-Output "running" } else { Write-Output "not running" }'
+        ScriptType      = 'PowerShell'
+        VM              = $Config.mountName
+        GuestCredential = $GuestCredential
+    }  
+    $output = Invoke-VMScript @splat -ErrorAction Stop
+    equals "$($output.Trim())" "running"
+}
 task .
